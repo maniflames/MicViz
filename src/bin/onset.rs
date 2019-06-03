f598df0dcf01e89696b77f0704002ab62ff948d5
Imani Dap <imani.dap@gmail.com>
2019-06-03 11:39:43 +0200

    chore(plp): add attempt to compute & display plp
    There are several problems with my implementation of this.
    There is a memory leak (idk where)
    Beats aren't detected for some lower frequencies
    Sine waves of some lower frequencies do not make sense
    Chosen BPM for most music doesn't feel intuitve
    
    This is definitely not the fault of this algorithm but of my limted math & dsp skills

--- src/bin/onset.rs
+++ src/bin/onset.rs
@@ -2,6 +2,7 @@ use portaudio;
 use std::sync::mpsc::*;
 use three;
 use std::collections::VecDeque;
+use clokwerk::{Scheduler, TimeUnits};
 
 //I should really consiuder putting this into a seperate crate because the scope isn't that small lmao
 
@@ -36,8 +37,9 @@ use std::collections::VecDeque;
 
 //a little more detail about onset detection http://resources.mpi-inf.mpg.de/departments/d4/teaching/ss2010/mp_mm/2010_MuellerGrosche_Lecture_MusicProcessing_BeatTracking_handout.pdf
 
-//TODO: print something on onset
-//TODO: create visualisation on onset
+//NOTE: bins range from 0 to half the sampeling rate, the number of bins is half the amount of the number of samples 
+// https://www.norwegiancreations.com/2017/08/what-is-fft-and-how-can-you-implement-it-on-an-arduino/
+
 fn main() {
     let pa = portaudio::PortAudio::new().expect("Unable to open PortAudio"); 
     let default_mic_index = pa.default_input_device().expect("Unable to get default device"); 
@@ -63,6 +65,8 @@ fn main() {
     let mut novelty_history: VecDeque<f64> = VecDeque::new(); 
     let mut normalised_novelty_history: VecDeque<f64> = VecDeque::new(); 
     let mut garbage_collection: Vec<three::Mesh> = Vec::new(); 
+    let mut sine_wave: VecDeque<f64> = VecDeque::new(); 
+    let mut last_peak: Vec<f64> = vec![0.0];
 
     let mut builder = three::Window::builder("A window Imani built"); 
     builder.fullscreen(true); 
@@ -71,12 +75,43 @@ fn main() {
 
     let camera = win.factory.orthographic_camera([0.0, 0.0], 1.0, -1.0 .. 1.0); 
 
+    let (sine_sender, sine_receiver): (SyncSender<i32>, Receiver<i32>) = sync_channel(2);
+    let mut sine_wave_scheduler = Scheduler::new();
+
+    sine_wave_scheduler.every(1.seconds()).run(move || {
+            match sine_sender.send(32) {
+                Ok(_) => (),
+                Err(err) => panic!("unable to schedule sine_wave. {:?}", err)
+            }
+        });
+
+    let mut was_sine_moved: bool = false; 
+
     while win.update() {
         match receiver.try_recv() {
             Ok(buffer) => calculate_novelty_curve(buffer, &mut history, &mut novelty_history, &mut normalised_novelty_history), 
             Err(_err) => ()
         }
-        draw_curve(&mut novelty_history, &mut normalised_novelty_history, &mut win, &mut garbage_collection);
+        draw_curve(&mut normalised_novelty_history, &mut win, &mut garbage_collection, false, &mut last_peak);
+
+        sine_wave_scheduler.run_pending();
+        match sine_receiver.try_recv() {
+            Ok(_) => {
+                calculate_plp_curve(&mut normalised_novelty_history, mic.default_sample_rate, &mut sine_wave);
+            },
+            Err(_) => ()
+        }
+
+        draw_curve(&mut sine_wave, &mut win, &mut garbage_collection, true, &mut last_peak);
+
+        if !was_sine_moved {
+            sine_wave.push_back(0.0); 
+            sine_wave.pop_front();
+            was_sine_moved = true;
+        } else {
+            was_sine_moved = false; 
+        }
+        
         win.render(&camera); 
         remove_lines(&mut win, &mut garbage_collection);
         garbage_collection.clear(); 
@@ -85,7 +120,7 @@ fn main() {
 
 fn calculate_novelty_curve(buffer: &[f32], history: &mut VecDeque<Vec<f64>>, novelty_history: &mut VecDeque<f64>, normalised_novelty_history: &mut VecDeque<f64>) {
     let samples: Vec<f64> = buffer.to_vec().into_iter().map(|sample| sample as f64).collect(); 
-    //Fourier Transform
+    //Fourier Transform, note that the output is in "nyquist bin" not "Hz"!
     let spectrum = meyda::get_amp_spectrum(&samples);
 
     //Log compression
@@ -96,6 +131,7 @@ fn calculate_novelty_curve(buffer: &[f32], history: &mut VecDeque<Vec<f64>>, nov
         }).collect(); 
 
     history.push_front(log_spectrum); 
+    
 
     if history.len() < 2 {
         return
@@ -117,14 +153,15 @@ fn calculate_novelty_curve(buffer: &[f32], history: &mut VecDeque<Vec<f64>>, nov
     let novelty_point = differentiation.iter().fold(0.0, |sum, difference| sum + difference);
     novelty_history.push_front(novelty_point);
 
-    if novelty_history.len() < 250 {
+    if novelty_history.len() < 128 //76 
+    { //novelty history length treshold
         return
     }
 
     let local_average = novelty_history.iter().fold(0.0, |sum, novelty_point| sum + novelty_point) / (novelty_history.len() as f64);
     let novelty_history_loop = novelty_history.clone(); //this is a memory hack and should be fixed dureing refactor
     //instead of picking peaks I just use a treshold
-    let treshold = 0.0; 
+    let treshold = 10.0; 
 
     normalised_novelty_history.clear();
     for novelty_point in novelty_history_loop {
@@ -142,8 +179,26 @@ fn calculate_novelty_curve(buffer: &[f32], history: &mut VecDeque<Vec<f64>>, nov
 }
 
 //TODO: return meshes that need to be removed 
-fn draw_curve(novelty_curve: &mut VecDeque<f64>, normalised_novelty_curve: &mut VecDeque<f64>, win: &mut three::window::Window, garbage_collection: &mut Vec<three::Mesh>) {
+fn draw_curve(normalised_novelty_curve: &mut VecDeque<f64>, win: &mut three::window::Window, garbage_collection: &mut Vec<three::Mesh>, is_fleeting: bool, last_peak: &mut Vec<f64>) {
     let curve = normalised_novelty_curve; 
+
+    let mut future_peaks: Vec<f64> = Vec::new(); 
+    let mut now = 0.0;
+
+    if is_fleeting {
+        let increment = (1.0 / curve.len() as f64) * 3.0;
+        now = 1.5 / increment; 
+
+        for (index, point) in curve.iter().enumerate().rev() {
+            if index == 0 || index == curve.len() - 1 {
+                continue;
+            }
+
+            if point > &curve[index - 1] && point > &curve[index + 1] && point > &0.0 && index >= now as usize {
+                future_peaks.push(*point); 
+            }
+        }
+    }
 
     for (index, novelty_point) in curve.iter().enumerate() {
         if index == 0 {
@@ -152,10 +207,16 @@ fn draw_curve(novelty_curve: &mut VecDeque<f64>, normalised_novelty_curve: &mut 
 
         let novelty_curve_len = curve.len() as f32; 
         let previous_index = (index - 1) as f32;
-        let previous_x = ((previous_index / novelty_curve_len) * 3.0) - 1.5; 
+        let mut previous_x = ((previous_index / novelty_curve_len) * 3.0) - 1.5; 
         let previous_y = curve[index - 1] as f32 / 100.0; 
-        let x = (((index as f32) / novelty_curve_len) * 3.0) - 1.5;
+        let mut x = (((index as f32) / novelty_curve_len) * 3.0) - 1.5;
         let y = *novelty_point as f32 / 100.0; 
+
+        //draw part of the sine_wave in the future 
+        if is_fleeting {
+            previous_x = previous_x + 1.5; 
+            x = x + 1.5; 
+        }
 
         //x from left to right is -1.5 to 1.5
         //y from bottom to top is 0.0 to 1.0
@@ -164,8 +225,35 @@ fn draw_curve(novelty_curve: &mut VecDeque<f64>, normalised_novelty_curve: &mut 
             [x, y - 0.5, 0.0].into()
         ]);
 
+        //if last point in future_peaks is found pop vec and detect beat 
+        if is_fleeting && index <= now as usize {
+            
+            //TODO: only detect if there is a peak in the novelty curve between the bases of the peak of the sine wave
+            for peak in future_peaks.clone() {
+                if novelty_point == &peak && novelty_point != &last_peak[0] {
+                    future_peaks.pop();
+
+                    last_peak.push(*novelty_point);
+                    last_peak.remove(0); 
+
+                    println!("beat! {:?}", peak); 
+
+                    if win.scene.background == three::Background::Color(0xFFFFFF) {
+                        win.scene.background = three::Background::Color(0x000000);
+                    } else {
+                        win.scene.background = three::Background::Color(0xFFFFFF);
+                    }
+                }
+            }
+        } 
+
+        let mut color = 0xFFFFFF; 
+        if is_fleeting {
+            color = 0xADD8E6; 
+        }
+
         let material = three::material::Line {
-            color: 0xFFFFFF,
+            color: color,
         };
 
         let mesh = win.factory.mesh(geometry, material);
@@ -178,4 +266,72 @@ fn remove_lines(win: &mut three::window::Window, garbage_collection: &mut Vec<th
     for mesh in garbage_collection {
         win.scene.remove(&mesh); 
     }
+}
+
+//NOTE: I'm note sure wether I should use the actual novelty numbers or the compressed novelty numbers.
+// numbers in the paper correspond to the compressed version. This is probably what is meant by 'further normalisation' 
+//TODO: FFT on (compressed) normalised novelty curve
+//NOTE: bins range from 0 to half the sampeling rate, the number of bins is half the amount of the number of samples 
+// https://www.norwegiancreations.com/2017/08/what-is-fft-and-how-can-you-implement-it-on-an-arduino/
+//TODO: bins as frequencies (as bpm)
+//TODO: draw sinusoid with the same frequency (not sure how yet language is very vague)
+
+//These are the steps for now, selecting multiple points and accumulating values only when this has succeeded 
+
+fn calculate_plp_curve(novelty_curve: &mut VecDeque<f64>, sample_rate: f64, sine_wave: &mut VecDeque<f64>) {
+    if novelty_curve.len() < 1 {
+        return;
+    }
+
+    //decided to compress with 100 after observing what's inside the normalised noverty history
+    let compressed_novelty_curve: VecDeque<f64> = novelty_curve.iter().map(|novelty_point| novelty_point / 100.0).collect();
+
+    //FFT to get tempogram
+    let spectrum = meyda::get_amp_spectrum(&Vec::from(compressed_novelty_curve.clone()));
+
+    //get frequencies of bin
+    let mut bins_as_frequencies: Vec<f64> = Vec::new();
+    let half_sample_rate = sample_rate / 2.0;
+    let linear_increment = half_sample_rate / (spectrum.len() as f64); 
+
+    for (index, _bin) in (0..spectrum.len()).into_iter().enumerate() {
+        &bins_as_frequencies.push((index as f64) * linear_increment); 
+    }
+
+    // NOTE: info could be useful in a later stadium
+    let bins_as_bpm: Vec<f64> = bins_as_frequencies.iter().map(|frequency| frequency / 60.0).collect();
+
+    // for (index, _bin) in (0..spectrum.len()).into_iter().enumerate() {
+    //     println!("{:?} bpm at {:?} intensity", bins_as_bpm[index], spectrum[index]);
+    // }
+
+    let mut peak: f64 = 0.0;
+    let mut peak_index = 0; 
+    for (index, intensity) in spectrum.iter().enumerate() {
+        if index == 0 || bins_as_bpm[index] < 40.0 || bins_as_bpm[index] > 200.0 {
+            continue; 
+        }
+
+        if peak.max(*intensity) == *intensity {
+            peak = *intensity;
+            peak_index = index; 
+            println!("{:?} bpm", bins_as_bpm[index]);
+        }
+    }
+
+    //with 76 as novelty history length treshold a peak is displayed +/- 1.6 seconds (mesured \w stopwatch)
+    // println!("cycles in period: {:?}, frequency {:?}", num_cycles, bins_as_frequencies[peak_index]); 
+
+    let time_increment = 3.6 / (bins_as_frequencies.len() as f64); //NOTE: this is hardcoded!!! used to be 1.6 
+    let mut bins_as_time: Vec<f64> = Vec::new();
+    for (index, _frequency) in bins_as_frequencies.iter().enumerate() {
+        bins_as_time.push((index as f64) * time_increment);
+    }
+
+    //amplitude = maxAmplitude * sin(frequency * time)
+    sine_wave.clear(); 
+     for (index, time) in bins_as_time.iter().enumerate() {
+        let frequency_time = bins_as_frequencies[peak_index] * time;
+        sine_wave.push_front(100.0 * frequency_time.sin()); 
+    }    
 }
